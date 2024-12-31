4277549ce45f09f6edaaa448f9003b3ad1f125be
Kareem <Kudo.200200@gmail.com>
2024-12-31 19:57:12 +0200

    Auth APIs Documentation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,14 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\AuthResource;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Knuckles\Scribe\Attributes\Response as ApiResponse;
+use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 
+/**
+ * @group Auth
+ */
 class AuthController extends Controller
 {
+    /**
+     * Login
+     */
+    #[ResponseFromApiResource(AuthResource::class, User::class)]
     public function login(LoginRequest $request): AuthResource
     {
         $user = User::firstWhere('email', $request->email);
@@ -26,6 +36,10 @@ class AuthController extends Controller
         return AuthResource::make($user);
     }
 
+    /**
+     * Register
+     */
+    #[ResponseFromApiResource(AuthResource::class, User::class)]
     public function register(RegisterRequest $request): AuthResource
     {
         $user = User::create($request->validated());
@@ -33,10 +47,23 @@ class AuthController extends Controller
         return AuthResource::make($user);
     }
 
+    /**
+     * Logout
+     */
+    #[ApiResponse(status: 204)]
     public function logout(Request $request): Response
     {
         $request->user()->currentAccessToken()->delete();
 
         return response()->noContent();
+    }
+
+    /**
+     * Me
+     */
+    #[ResponseFromApiResource(UserResource::class, User::class)]
+    public function me(Request $request): UserResource
+    {
+        return UserResource::make($request->user());
     }
 }
